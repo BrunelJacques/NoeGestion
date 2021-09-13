40cb6b621750e6117baedff8d133441e14e816ce
Jacques <jbrunel@laposte.net>
2021-09-13 22:11:19 +0200

    Tables Django

--- appli_web/models.py
+++ appli_web/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# Describe your models here.
+
+class geAnalytiques(models.Model):
+    # Etat des stocks à une date donnée
+    idAnalytique = models.CharField(max_lenght=8, blank=True, unique=True,
+                                    db_index=True)
+    label = models.CharField(max_lenght=200, blank=True,
+                             help_text="Libellé long du code analytique"
+                             )
+    params = models.TextField( blank=True,
+                               help_text="liste texte de paramétrages constructeurs, pour le calcul coût"
+                               )
+    axe = models.CharField(max_lenght=32, blank=True,
+                           help_text="axe analytique 'VEHICULES' 'CONVOIS' 'PRIXJOUR', defaut = vide"
+                           )
+    dateSaisie = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return "%s %s" % (self.idAnalytique,self.label)
+
+    class Meta:
+        ordering = ['idAnalytique',]
+
 
 class stArticles(models.Model):
     # articles utilisés pour les stocks
@@ -24,23 +46,50 @@ class stArticles(models.Model):
     ]
 
     idArticle = models.CharField(max_lenght=128, blank=True, unique=True,
-                                 db_index=True)
-    rations = models.DecimalField(blank=True)
+                                 db_index=True,
+                                 help_text="PK Désignation du produit",
+                                 )
+    rations = models.DecimalField(blank=True,
+                                  help_text="Nombre de ration pour une unité"
+                                  )
     fournisseur = models.CharField(max_lenght=32, blank=True,
-                                   db_index=True)
-    qteStock = models.IntegerField( null=False, default=0)
-    txTva = models.DecimalField(blank=False)
+                                   db_index=True,
+                                   help_text="Fournisseur habituel"
+                                   )
+    qteStock = models.IntegerField( null=False, default=0,
+                                    help_text="Stock en live"
+                                    )
+    txTva = models.DecimalField(blank=False,
+                                help_text="tx de TVA en %"
+                                )
     magasin = models.IntegerField( blank=False, choices=MAGASIN_CHOICES,
-                                   db_index=True)
+                                   db_index=True,
+                                   help_text="Lieu de stockage: réserve, congel,frigo"
+                                   )
     rayon = models.IntegerField( blank=False, choices=RAYON_CHOICES,
-                                 db_index=True)
-    qteMini = models.IntegerField( null=False, default=0)
-    qteSaison = models.IntegerField( null=False, default=0)
-    obsolete = models.BooleanField( null=False, default=False)
-    prixMoyen = models.DecimalField(blank=True)
-    prixActuel = models.DecimalField(blank=True)
-    dernierAchat = models.DateField(blank=False)
-    ordi = models.CharField(max_lenght=32, blank=True)
+                                 db_index=True,
+                                 help_text="rayon ou famille produit: type de produit dans le magasin"
+                                 )
+    qteMini = models.IntegerField( null=False, default=0,
+                                   help_text="Seuil déclenchant une alerte rouge"
+                                   )
+    qteSaison = models.IntegerField( null=False, default=0,
+                                     help_text="Seuil souhaitable en haute saison"
+                                     )
+    obsolete = models.BooleanField( null=False, default=False,
+                                    help_text="1 article qui n'est plus utilisé"
+                                    )
+    prixMoyen = models.DecimalField(blank=True,
+                                    help_text="Prix unitaire moyen historique du stock"
+                                    )
+    prixActuel = models.DecimalField(blank=True,
+                                     help_text="Dernier prix TTC unitaire livré ou de réappro"
+                                     )
+    dernierAchat = models.DateField(blank=False,
+                                    help_text="Date de dernière entrée avec prix saisi"
+                                    )
+    ordi = models.CharField(max_lenght=32, blank=True,
+                            help_text= "Nom de l'ordi utilisé entrée ou modif")
     dateSaisie = models.DateField(auto_now=True)
 
     @property
@@ -56,17 +105,34 @@ class stArticles(models.Model):
 
 class stEffectifs(models.Model):
     # articles utilisés pour les stocks
-    idAnalytique = models.CharField(max_lenght=32, blank=True,
-                                    db_index=True)
+    idAnalytique = models.ForeignKey(geAnalytiques,
+                                     on_delete=models.RESTRICT,
+                                     help_text="PK Section analytique du camp à facturer null pour Cuisine"
+                                     )
     idDate = models.DateField(blank=False,
-                              db_index=True)
-    midiClients = models.IntegerField( null=False, default=0)
-    midiRepas = models.IntegerField( null=False, default=0)
-    soirClients = models.IntegerField( null=False, default=0)
-    soirRepas = models.IntegerField( null=False, default=0)
-    prevuClients = models.IntegerField( null=False, default=0)
-    prevuRepas = models.IntegerField( null=False, default=0)
-    ordi = models.CharField(max_lenght=32, blank=True)
+                              db_index=True,
+                              help_text="PK Date de la situation de l'effectif"
+                              )
+    midiClients = models.IntegerField( null=False, default=0,
+                                       help_text="Nbre de repas midi clients facturés "
+                                       )
+    midiRepas = models.IntegerField( null=False, default=0,
+                                     help_text="Nbre de repas midi pour le staff et les clients"
+                                     )
+    soirClients = models.IntegerField( null=False, default=0,
+                                       help_text="Nbre de repas aux clients présents le soir "
+                                       )
+    soirRepas = models.IntegerField( null=False, default=0,
+                                     help_text="Nbre de repas pour le staff et les clients le soir"
+                                     )
+    prevuClients = models.IntegerField( null=False, default=0,
+                                        help_text="Nbre d'inscrits payants "
+                                        )
+    prevuRepas = models.IntegerField( null=False, default=0,
+                                      help_text="Nbre d'inscrits staff inclus"
+                                      )
+    ordi = models.CharField(max_lenght=32, blank=True,
+                            help_text= "Nom de l'ordi utilisé entrée ou modif")
     dateSaisie = models.DateField(auto_now=True)
 
     @property
@@ -77,13 +143,14 @@ class stEffectifs(models.Model):
             preparations = 1 # pas de repas midi ou soir
         return (self.midiClients + self.soirClients) / preparations
 
+    def __str__(self):
+        return self.idDate
+
     @property
     def repas(self):
         # total des repas préparés
         return self.midiRepas + self.soirRepas
 
-    def __str__(self):
-        return '%s  %s'%(idAnalytique, str(idDate))
 
     class Meta:
         db_table = 'stEffectifs'
@@ -92,6 +159,21 @@ class stEffectifs(models.Model):
 class stMouvements(models.Model):
     # mouvements de sortie ou d'entrée en stock
 
+    ORIGINE_CHOICES = [
+        ('entrees',(
+                ('achat','Achats'),
+                ('retour','Retour de camp'),
+                ('od_in','Correctif entrée'),
+            )
+        ),
+        ('sorties',(
+            ('repas','Repas en cuisine'),
+            ('camp','Sortie pour camp'),
+            ('od_out','Correctif sortie')
+            )
+        )
+    ]
+
     REPAS_CHOICES = [
         (1, 'Matin'),
         (2, 'Midi'),
@@ -99,20 +181,44 @@ class stMouvements(models.Model):
         (4, 'Tous'),
     ]
 
-    idAnalytique = models.CharField(max_lenght=32, blank=True)
-    idDate = models.DateField(blank=False)
-    idArticle = models.CharField(max_lenght=128, blank=True)
-    fournisseur = models.CharField(max_lenght=32, blank=True)
-    origine = models.CharField(max_lenght=32, blank=True)
-    qte = models.IntegerField( null=False, default=0)
-    prixUnit = models.DecimalField(blank=False)
-    repas = models.IntegerField( blank=False, choices=REPAS_CHOICES,)
-    ordi = models.CharField(max_lenght=32, blank=True)
+    idAnalytique = models.ForeignKey(geAnalytiques,
+                                     on_delete=models.RESTRICT,
+                                     null=True,
+                                     db_index=True,
+                                     help_text="Section analytique du camp à facturer, nulle pour cuisine"
+                                     )
+    idDate = models.DateField(blank=False,
+                              db_index=True,
+                              help_text="date du mouvement de stock"
+                              )
+    idArticle = models.ForeignKey(stArticles,
+                                  on_delete=models.RESTRICT,
+                                  db_index=True,
+                                  help_text="article mouvementé"
+                                  )
+    fournisseur = models.CharField(max_lenght=32, blank=True,
+                                   db_index=True,
+                                   help_text="Fournisseur de l'entrée"
+                                   )
+    origine = models.CharField(blank=False, choices=ORIGINE_CHOICES,
+                               help_text="Origine du mouvement")
+    qte = models.DecimalField( null=False, default=0,
+                               help_text="Quantitée mouvementée signée"
+                               )
+    prixUnit = models.DecimalField(blank=False,
+                                   help_text= "Prix moyen pour sorties et retour, Prix revient pour achats"
+                                )
+    repas = models.IntegerField( blank=False, choices=REPAS_CHOICES,
+                                 help_text="Repas concerné par la sortie vers cuisine")
+    ordi = models.CharField(max_lenght=32, blank=True,
+                            help_text="Nom de l'ordi utilisé entrée ou modif")
     dateSaisie = models.DateTimeField(auto_now=True)
-    modifiable = models.BooleanField(default=True)
+    modifiable = models.BooleanField(default=True,
+                                     help_text= "0/1 Marque un transfert export  réussi ou import"
+                                    )
 
     def __str__(self):
-        return '%s  %s'%(idAnalytique, str(idDate),)
+        return '%s  %s' % (self.idAnalytique, str(self.idDate),)
 
     class Meta:
         db_table = 'stMouvements'
@@ -121,78 +227,38 @@ class stMouvements(models.Model):
 class stInventaires(models.Model):
     # Etat des stocks à une date donnée
 
-    idDate = models.DateField(blank=False)
-    idArticle = models.CharField(max_lenght=128, blank=True)
-    qteStock = models.IntegerField(null=False, default=0)
-    qteConstat = models.IntegerField(null=False, default=0)
-    prixMoyen = models.DecimalField(blank=True)
-    prixActuel = models.DecimalField(blank=True)
-    ordi = models.CharField(max_lenght=32, blank=True)
+    idDate = models.DateField(blank=False,
+                              db_index=True,
+                              help_text="PK Date de l'inventaire copie des stocks confirmée"
+                              )
+    idArticle = models.ForeignKey(stArticles,
+                                  on_delete=models.RESTRICT,
+                                  db_index=True,
+                                  help_text="PK Désignation du produit"
+                                  )
+    qteStock = models.IntegerField(null=False, default=0,
+                                   help_text="Qté reportée par calcul depuis dernier constat"
+                                   )
+    qteConstat = models.IntegerField(null=False, default=0,
+                                     help_text="Qté constatée lors d'un inventaire"
+                                     )
+    prixMoyen = models.DecimalField(blank=True,
+                                    help_text="Prix unitaire moyen historique du stock"
+                                    )
+    prixActuel = models.DecimalField(blank=True,
+                                     help_text="Prix forcé pour valorisation de l'inventaire"
+                                     )
+    ordi = models.CharField(max_lenght=32, blank=True,
+                            help_text="Nom de l'ordi utilisé entrée ou modif")
     dateSaisie = models.DateField(auto_now=True)
-    modifiable = models.BooleanField(default=True)
+    modifiable = models.BooleanField(default=True,
+                                     help_text="0/1 Marque un transfert export  réussi ou import"
+                                     )
 
     def __str__(self):
-        return idDate
+        return self.idDate
 
     class Meta:
         db_table = 'stInventaires'
         ordering = ['idDate','idArticle',]
 
-
-class analytiques(models.Model):
-    # Etat des stocks à une date donnée
-    idAnalytique = models.CharField(max_lenght=8, blank=True, unique=True,
-                                    db_index=True)
-    label = models.CharField(max_lenght=200, blank=True)
-    params = models.TextField( blank=True)
-    axe = models.CharField(max_lenght=32, blank=True)
-    dateSaisie = models.DateField(auto_now=True)
-
-    def __str__(self):
-        return "%s"%self.idAnalytique
-
-    class Meta:
-        db_table = 'analytiques'
-        ordering = ['idAnalytique',]
-
-
-
-class activites(models.Model):
-    # classe test
-    IDactivite = models.AutoField(db_column='IDactivite', primary_key=True)
-    nom = models.CharField(max_length=200, blank=True, null=True)
-    abrege = models.CharField(max_length=50, blank=True, null=True)
-    coords_org = models.IntegerField(blank=True, null=True)
-    rue = models.CharField(max_length=200, blank=True, null=True)
-    cp = models.CharField(max_length=10, blank=True, null=True)
-    ville = models.CharField(max_length=100, blank=True, null=True)
-    tel = models.CharField(max_length=50, blank=True, null=True)
-    fax = models.CharField(max_length=50, blank=True, null=True)
-    mail = models.CharField(max_length=100, blank=True, null=True)
-    site = models.CharField(max_length=100, blank=True, null=True)
-    logo_org = models.IntegerField(blank=True, null=True)
-    logo = models.TextField(blank=True, null=True)
-    date_debut = models.CharField(max_length=10, blank=True, null=True)
-    date_fin = models.CharField(max_length=10, blank=True, null=True)
-    public = models.CharField(max_length=20, blank=True, null=True)
-    vaccins_obligatoires = models.IntegerField(blank=True, null=True)
-    date_creation = models.CharField(max_length=10, blank=True, null=True)
-    nbre_inscrits_max = models.IntegerField(blank=True, null=True)
-    code_comptable = models.TextField(blank=True, null=True)
-
-    # ajout selon 'https://www.univ-orleans.fr/iut-orleans/informatique
-    # /intra/tuto/django/django-models.html'
-    def __str__(self):
-        return self.nom
-
-    @property
-    def categorie(self):
-        return self.abrege[-2:]
-
-    @property
-    def annee(self):
-        return self.abrege[4:]
-
-    class Meta:
-        managed = True
-        db_table = 'activites'
