dc20877e6f7355cfc7fbc8c3e5922ea95f14ae36
Jacques <jbrunel@laposte.net>
2021-09-19 19:33:16 +0200

    Tables Django

--- appli_web/urls.py
+++ appli_web/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+
+
 urlpatterns=[
   path('home',views.home,name='home'),
   path('home/<name>', views.home, name='home'),
   path('', views.home, name='home'),
+  path('auth/login/', obtain_jwt_token),
+  path('auth/refresh-token/', refresh_jwt_token),
 ]
