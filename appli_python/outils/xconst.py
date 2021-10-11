87ed3132548b355910d97e5fce2da686e06f348c
Jacques <jbrunel@laposte.net>
2021-10-11 22:05:24 +0200

    Importation des outils

---
+++ appli_python/outils/xconst.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#  Jacques Brunel
+#  MATTHANIA - Projet NoeGestion - xconst.py (constantes)
+#  2021-10-11
+
+SYMBOLE = "€"
+
+LISTE_JOURS = ["Lundi", "Mardi", "Mercredi","Jeudi","Vendredi", "Samedi", "Dimanche"]
+
+LISTE_MOIS = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet","Août",
+              "Septembre", "Octobre", "Novembre", "Décembre"]
+
+LISTE_NB_MINI_JOURS_MOIS = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+LISTE_MOIS_ABREGE =[("Janv."), ("Fév."), ("Mars"), ("Avril"), ("Mai"), ("Juin"), ("Juil."),
+                    ("Août"), ("Sept."), ("Oct."), ("Nov."), ("Déc.")]
