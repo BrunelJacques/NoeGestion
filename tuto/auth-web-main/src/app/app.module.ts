c7504404ad6885617dc4b31f6d8c3c78428ff4f5
Jacques <jbrunel@laposte.net>
2023-01-18 16:51:02 +0100

    Tuto mattermost

---
+++ tuto/auth-web-main/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { UserLoginComponent } from './user-login/user-login.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TokenInterceptor } from "./token.interceptor";
+import { MatCardModule } from "@angular/material/card";
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserLoginComponent,
+    UserProfileComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  providers: [
+     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+   ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
