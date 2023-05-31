310b75075683a7fd4677e07b5406aafba1df0a97
Jacques <jbrunel@laposte.net>
2023-05-31 22:27:16 +0200

    Echanges django-angular

--- web-noegest/src/app/general/_services/authentication.service.ts
+++ web-noegest/src/app/general/_services/authentication.service.ts
@@ -1,6 +1,6 @@
 ﻿import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -12,6 +12,7 @@ export class AuthenticationService {
     private cst = new Constantes
     private userSubject: BehaviorSubject<User | null>;
     public user: Observable<User | null>;
+    private refreshtkn : string
 
 
     constructor(
@@ -26,9 +27,28 @@ export class AuthenticationService {
         return this.userSubject.value;
     }
 
- 
+
     login(username: string, password: string) {
-        return this.http.post<any>(this.cst.TOKEN_URL, { username, password }, { withCredentials: false })
+        return this.http.post<any>(
+            this.cst.TOKEN_URL, 
+            { username, password }, 
+            { withCredentials: false}
+            ).pipe(map(user => {
+                user.jwtToken = user.access;
+                this.refreshtkn = user.refresh;
+                this.userSubject.next(user);
+                this.startRefreshTokenTimer();
+                return user;
+            }));
+    }
+
+    refreshToken() {
+        console.log(this.refreshToken)
+        return this.http.post<any>(
+                this.cst.TOKENREFRESH_URL,
+                { refresh: this.refreshtkn },
+                { withCredentials: false }
+                )
             .pipe(map(user => {
                 user.jwtToken = user.access;
                 this.userSubject.next(user);
@@ -37,6 +57,7 @@ export class AuthenticationService {
             }));
     }
 
+    
     logout() {
         this.http.post<any>(this.cst.API_URL+'/logout/', {}, { withCredentials: true }).subscribe();
         this.stopRefreshTokenTimer();
@@ -44,14 +65,7 @@ export class AuthenticationService {
         this.router.navigate(['/login']);
     }
 
-    refreshToken() {
-        return this.http.post<any>(this.cst.TOKENREFRESH_URL, {}, { withCredentials: true })
-            .pipe(map((user) => {
-                this.userSubject.next(user);
-                this.startRefreshTokenTimer();
-                return user;
-            }));
-        }
+
 
     register(user: User) {
         console.log(this.cst.API_URL+'/register',user)
@@ -62,7 +76,7 @@ export class AuthenticationService {
 
     private refreshTokenTimeout?: NodeJS.Timeout;
 
-    
+
     private startRefreshTokenTimer() {
         // parse json object from base64 encoded jwt token
         const jwtBase64 = this.userValue!.jwtToken!.split('.')[1];
@@ -70,13 +84,13 @@ export class AuthenticationService {
 
         // set a timeout to refresh the token a minute before it expires
         const expires = new Date(jwtToken.exp * 1000);
-        const timeout = expires.getTime() - Date.now() - (60 * 1000);
-        //const timeout = expires.getTime() - Date.now() - (60 * 1000) - 220000; //pour test
+        //const timeout = expires.getTime() - Date.now() - (60 * 1000);
+        const timeout = expires.getTime() - Date.now() - (60 * 1000) - 220000; //pour test
         this.refreshTokenTimeout = setTimeout(() => this.refreshToken().subscribe(), timeout);
     }
 
     private stopRefreshTokenTimer() {
         clearTimeout(this.refreshTokenTimeout);
     }
- 
+
 }
