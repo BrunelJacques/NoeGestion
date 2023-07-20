e29bfaf1060911bbf05ac52f0b41a9943786a92d
BrunelJacques <jbrunel@laposte.net>
2023-07-20 21:43:23 +0200

    gestion des boutons subheader-mvt

---
+++ web-noegest/src/app/general/_services/shared.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { BehaviorSubject, filter } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+
+export class SharedService {
+  public onSubmitEvent = new EventEmitter(undefined);
+  public onGoBackEvent = new EventEmitter(undefined);
+
+  public urlActive$ = new BehaviorSubject<string>("")
+  public rootActive$ = new BehaviorSubject<string>("")
+  public templateActive$ = new BehaviorSubject<string>("")
+
+  urlsHisto: string[] = []
+
+  constructor( 
+    private router: Router 
+    ){
+    this.router.events
+      .pipe( filter(event => event instanceof NavigationEnd),)
+      .subscribe(() => {this.updateUrl()})
+    }
+
+  updateUrl(){
+    this.urlActive$.next(this.router.url)
+    const tblUrl = this.router.url.split('/')
+    if (
+      (tblUrl.length > 1) && (tblUrl[1].length >1)
+      )
+    { this.rootActive$.next(tblUrl[1]) 
+      this.templateActive$.next(tblUrl[2])
+    } 
+    else { 
+      this.rootActive$.next('-') 
+      this.templateActive$.next('-')
+    }
+  }
+
+  setUrlParent(){
+    const url = this.router.url
+    if (this.urlsHisto[0] != url)
+    {this.urlsHisto.unshift(url)}
+    console.log('hello setUrlParent',url)
+  }
+
+  goBackUrlParent() {
+    //route vers le dernier parent inséré et le supprime
+    const url = this.urlsHisto.shift()
+    this.router.navigate([url])
+  }
+}
