a5a4951fb6ed7d599aedd23210c258aa81744e92
BrunelJacques <jbrunel@laposte.net>
2023-10-27 09:22:23 +0200

    one-sortie recherche article

--- web-noegest/src/app/stocks/stocks.module.ts
+++ web-noegest/src/app/stocks/stocks.module.ts
@@ -9,21 +9,31 @@ import { EntreesComponent } from './mouvements/entrees/entrees.component';
 import { OneEntreeComponent } from './mouvements/one-entree/one-entree.component';
 import { SortiesComponent } from './mouvements/sorties/sorties.component';
 import { PrixJourneeComponent } from './prix-journee/prix-journee.component';
-
+import { OneSortieComponent } from './mouvements/one-sortie/one-sortie.component';
+import { ArticleSearchComponent } from './mouvements/article-search/article-search.component';
+import { ParamsComponent } from './params/params.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        StocksRoutingModule,
-    ],
-    declarations: [
-        InventaireComponent,
-        EffectifsComponent,
-        EntreesComponent,
-        OneEntreeComponent,
-        SortiesComponent,
-        PrixJourneeComponent,
-    ],
+  declarations: [
+    ParamsComponent,
+    InventaireComponent,
+    EffectifsComponent,
+    EntreesComponent,
+    OneEntreeComponent,
+    SortiesComponent,
+    OneSortieComponent,
+    PrixJourneeComponent,
+    ArticleSearchComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    StocksRoutingModule,
+  ],
+  exports: [
+    OneSortieComponent,
+    ArticleSearchComponent,
+    ParamsComponent,
+  ]
 })
 export class StocksModule { }
