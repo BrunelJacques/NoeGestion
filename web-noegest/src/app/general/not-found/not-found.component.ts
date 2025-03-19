ccab57b9173a2a500b1813175fe5070e2b81a604
BrunelJacques <jbrunel@laposte.net>
2025-03-19 22:05:13 +0100

    projet noegest display

--- web-noegest/src/app/general/not-found/not-found.component.ts
+++ web-noegest/src/app/general/not-found/not-found.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
   title = "URL not found : ";
