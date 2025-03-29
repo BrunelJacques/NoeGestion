eef68b4644c896bba7583c3b1959e4e372501d6f
BrunelJacques <jbrunel@laposte.net>
2025-03-29 17:06:00 +0100

    projet noegest display

---
+++ web-noegest/src/app/general/subheader/subheader-register/subheader-register.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { SeeyouService } from 'src/app/general/_services';
+import { User } from '../../_models';
+import { AuthenticationService } from 'src/app/general/_services';
+@Component({
+  selector: 'app-subheader-register',
+  templateUrl: './subheader-register.component.html',
+})
+
+export class SubheaderRegisterComponent {
+  parentName = "-"
+  nomModule = "-"
+  isBtnQuit = true
+  libBtnOk = "Ok"
+  template = "."
+  valid = true
+  user!: User
+
+  constructor(
+    private seeyouService: SeeyouService,
+    private authenticationService: AuthenticationService
+  ){
+    this.seeyouService.templateActive$.subscribe(
+      (template) => { 
+        this.template = template
+        this.parentName = this.seeyouService.getParentName()
+      }
+    )
+
+    this.user = this.authenticationService.userValue;
+  }
+
+  onClickQuit() {
+    this.seeyouService.emitClickQuit()
+  }
+
+}
